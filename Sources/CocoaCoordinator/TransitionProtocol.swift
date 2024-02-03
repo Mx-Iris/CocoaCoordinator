e13e835b90d231be1488b1286ff36d7ab2492371
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-02-03 18:35:07 +0800

    Add missing functions

--- Sources/CocoaCoordinator/TransitionProtocol.swift
+++ Sources/CocoaCoordinator/TransitionProtocol.swift
@@ -6,7 +6,21 @@ public protocol TransitionProtocol: TransitionContext {
     associatedtype W
     associatedtype V
     func perform(on windowController: W?, in viewController: V?, with options: TransitionOptions, completion: PresentationHandler?)
+    static func multiple(_ transitions: [Self]) -> Self
 }
 
+extension TransitionProtocol {
+
+    ///
+    /// Creates a compound transition by chaining multiple transitions together.
+    ///
+    /// - Parameter transitions:
+    ///     The transitions to be chained to form a combined transition.
+    ///
+    public static func multiple(_ transitions: Self...) -> Self {
+        return multiple(transitions)
+    }
+
+}
 
 #endif
