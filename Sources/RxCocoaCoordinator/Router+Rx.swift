b0c68db8f6c64bafccd9d4ed257215efc60f14a3
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-06-23 02:40:53 +0800

    Updates

--- Sources/RxCocoaCoordinator/Router+Rx.swift
+++ Sources/RxCocoaCoordinator/Router+Rx.swift
@@ -2,33 +2,44 @@ import RxSwift
 import RxCocoa
 import CocoaCoordinator
 
-extension Reactive where Base: Router & AnyObject {
-    public func trigger<Route: Routable>() -> Binder<Route> where Route == Base.Route {
-        .init(base) { router, route in
+public class ReactiveRouter<Route: Routable> {
+    // MARK: Stored Properties
+
+    public let base: any Router<Route>
+
+    // MARK: Initialization
+
+    public init(_ base: any Router<Route>) {
+        self.base = base
+    }
+}
+
+extension Router {
+    /// Use this to access the reactive extensions of `Router` objects.
+    public var rx: ReactiveRouter<Route> {
+        // swiftlint:disable:previous identifier_name
+        ReactiveRouter(self)
+    }
+}
+
+extension ReactiveRouter {
+    public func trigger() -> Binder<Route> {
+        .init(self) { router, route in
             Task {
                 await MainActor.run {
-                    router.trigger(route)
+                    router.base.trigger(route)
                 }
             }
         }
     }
     
-    public func trigger<Route: Routable>(_ route: Route) -> Binder<Void> where Route == Base.Route {
-        .init(base) { router, _ in
+    public func trigger(_ route: Route) -> Binder<Void> {
+        .init(self) { router, _ in
             Task {
                 await MainActor.run {
-                    router.trigger(route)
+                    router.base.trigger(route)
                 }
             }
         }
-    }
-}
-
-extension Router {
-
-    /// Use this to access the reactive extensions of `Router` objects.
-    public var rx: Reactive<Self> {
-        // swiftlint:disable:previous identifier_name
-        return Reactive(self)
     }
 }
